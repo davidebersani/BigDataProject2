636bef62ad9e9a7d9f54efa7f4f6b25123007b71
Davide Bersani <davide.bersani96@gmail.com>
2020-06-21 15:47:40 +0200

    Updated readme. Updated init script.

--- README.md
+++ README.md
@@ -5,11 +5,10 @@
 Before run the benchmarks, it's necessary to initialize the project.
 Run the following commands.
 
-**N.B. The path to the directory where you download the repo must be without spaces.**
+**N.B. The path to the directory where you download the repo can't contain spaces.**
 ```
 wget <address>
 cd <dir>
-mkdir scripts
 ./init-project.sh
 ```
 Afther this, you can download YCSB using the script download-ycsb.sh. Then, you're ready to benchmark!
--- init-project.sh
+++ init-project.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
 # Script to set the correct path in the scripts.
 dir="$(pwd)"
+mkdir scripts
 for filename in template-scripts/*; do
     name="$(basename $filename)"
     sed "s@=REPO_DIR=@$dir@g" $filename > scripts/$name
 done
 
-
-
-# if [ "$1" = "help" -o "$#" -ne 2 ]
-# then
-#   echo "Usage: ./init-project.sh <path of this dir> <nome immagine Docker, sarà il suffisso di esperimentesi/>."
-# else
-#   cd "$1"
-#   echo "==> Avvio build del servizio in $1"
-#   gradle build && \
-
-#   echo "==> Avvio build dell'immagine Docker" && \
-#   docker build . -t esperimentesi/"$2" && \
-
-#   echo "==> Carico l'immagine Docker su DockerHub" && \
-#   docker push esperimentesi/"$2" && \
-
-#   echo "==> Avvio il rollout"
-#   # Si presuppone che i deployment si chiamino <nome immagine>-deployment
-#   kubectl rollout restart deployment/"$2"-deployment -n template-ns && \
-#   echo "==> Rollout avviato, controlla il completamento"
-# fi
