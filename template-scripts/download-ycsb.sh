8203c75420447b2e433c2f8d441227219d71df43
andrea19081996 <33671536+andrea19081996@users.noreply.github.com>
2020-06-27 16:35:14 +0200

    update remove ycsb tar

--- template-scripts/download-ycsb.sh
+++ template-scripts/download-ycsb.sh
@@ -5,3 +5,4 @@ echo "==> Downlaod YCSB v. 0.17.0"
 curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz
 echo "==> Extracting..."
 tar xfvz ycsb-0.17.0.tar.gz --directory $REPO_HOME
+rm -r ycsb-0.17.0.tar.gz
