23b68fdf51b9a257abafcc98d02f4771d7277933
Davide Bersani <davide.bersani96@gmail.com>
2020-07-02 12:05:14 +0200

    Added support for multi clients

--- template-scripts/benchmark-mongodb-a.py
+++ template-scripts/benchmark-mongodb-a.py
@@ -1,6 +1,6 @@
 # Script for execute workload A on Mongodb with various parameters.
 import subprocess
-import plots
+# import plots
 import sys
 
 REPO_HOME="=REPO_DIR="
@@ -10,13 +10,16 @@ SCRIPTS="=REPO_DIR=/scripts"
 mongodb_connection_string = "mongodb://localhost:27017/ycsb?w=1"
 output_dir="=REPO_DIR=/scripts/test-mongo-scenarioA"
 
-def getOutputFilename(operation, dim, t) :
+def getOutputFilename(operation, dim, t, c) :
     filename = output_dir + "/output-" + operation
     if dim is not None:
         filename = filename + "-dim" + str(dim)
 
     if t is not None:
         filename = filename + "-throughput" + str(t)
+    
+    if c is not None:
+        filename = filename + "-clients" + str(c)
 
     filename = filename + ".txt"
     return filename
@@ -43,11 +46,11 @@ def delete_db(usage) :
 def laod_data(dim) :
     # Execute workload a for every dim
     print("\n\n==> Loading data: " + str(dim) + " record")
-    command = BIN_YCSB + "/ycsb.sh load mongodb-async -s -P " + HOME_YCSB + "/workloads/workloada -p recordcount=" + str(dim) + " -p mongodb.url=" + mongodb_connection_string + " > " + getOutputFilename("Load", dim, None)
+    command = BIN_YCSB + "/ycsb.sh load mongodb-async -s -P " + HOME_YCSB + "/workloads/workloada -p recordcount=" + str(dim) + " -p mongodb.url=" + mongodb_connection_string + " > " + getOutputFilename("Load", dim, None, None)
     execute(command)
 
-def run_workload(dim, t) :
-    command = BIN_YCSB + "/ycsb.sh run mongodb-async -s -P " + HOME_YCSB + "/workloads/workloada -p recordcount=" + str(dim) + " -target " + str(t) + " -p mongodb.url=" + mongodb_connection_string + " > " + getOutputFilename("Run", dim, t)
+def run_workload(dim, t, c) :
+    command = BIN_YCSB + "/ycsb.sh run mongodb-async -s -P " + HOME_YCSB + "/workloads/workloada -p recordcount=" + str(dim) + " -threads " + str(c) +" -target " + str(t) + " -p mongodb.url=" + mongodb_connection_string + " > " + getOutputFilename("Run", dim, t, c)
     execute(command)
 
 
@@ -59,19 +62,20 @@ if len(sys.argv)<2 :
 
 
 # Every record is 1KB (10 fields of 100B)
-input_dim = [1000, 10000, 100000, 500000, 1000000]
-throughput = [100, 1000, 100000]
-#clients=[]
+input_dim = [100000, 500000, 1000000, 3000000, 5000000]
+throughput = [1000, 100000, 1000000]
+clients=[10]
 
-for dim in input_dim:
-    laod_data(dim)
-    for t in throughput:
-        print("\n==> Wordload A. Input dim: " + str(dim) + "; Throughput: " + str(t))
-        run_workload(dim, t)
-    
-    delete_db(sys.argv[1])
+for num_clients in clients:  
+    for dim in input_dim:
+        laod_data(dim)
+        for t in throughput:
+            print("\n==> Wordload A. Input dim: " + str(dim) + "; Throughput: " + str(t) + "; Clients: " + str(num_clients))
+            run_workload(dim, t, num_clients)
+        
+        delete_db(sys.argv[1])
 
 # PROSSIMI PASSI: AGGIUNGERE ITERAZIONE SUI CLIENT.
 
-plots.generateAndShowInputLatencyPlots(input_dim, throughput, "test-mongo-scenarioA")
-plots.generateAndShowThroughputLatencyPlots(input_dim, throughput, "test-mongo-scenarioA")
+# plots.generateAndShowInputLatencyPlots(input_dim, throughput, clients, "test-mongo-scenarioA")
+# plots.generateAndShowThroughputLatencyPlots(input_dim, throughput, clients, "test-mongo-scenarioA")
