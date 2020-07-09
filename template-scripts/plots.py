f3f2720444fac17d31486f0bc83e8e4e157b47e4
Davide Bersani <davide.bersani96@gmail.com>
2020-07-09 15:33:09 +0200

    Added scan and read-modify-write operations in plots

--- template-scripts/plots.py
+++ template-scripts/plots.py
@@ -24,6 +24,8 @@ def generateAndShowInputLatencyPlots(input_dim, throughputs, clients, source, wo
             # Generate plot Input dim / latency for every throughput value
             read = Serie("read")
             update = Serie("update")
+            scan = Serie("scan")
+            readModifyWrite = Serie("Read-Modify-Write")
         
             for dim in input_dim:
                 # new_path = os.path.join(cur_path, source ,"output-Run-dim" + str(dim) +".txt")
@@ -36,17 +38,36 @@ def generateAndShowInputLatencyPlots(input_dim, throughputs, clients, source, wo
                         parts = line.split("[READ], AverageLatency(us), ")
                         num = float(parts[1])
                         read.addPoint(dim, num)
-                    else:
-                        if "[UPDATE], AverageLatency(us)," in line:
-                            parts = line.split("[UPDATE], AverageLatency(us),")
-                            num = float(parts[1])
-                            update.addPoint(dim, num)
+                    elif "[UPDATE], AverageLatency(us)," in line:
+                        parts = line.split("[UPDATE], AverageLatency(us),")
+                        num = float(parts[1])
+                        update.addPoint(dim, num)
+                    elif "[SCAN], AverageLatency(us)," in line:
+                        parts = line.split("[SCAN], AverageLatency(us),")
+                        num = float(parts[1])
+                        scan.addPoint(dim, num)
+                    elif "[READ-MODIFY-WRITE], AverageLatency(us)," in line:
+                        parts = line.split("[READ-MODIFY-WRITE], AverageLatency(us),")
+                        num = float(parts[1])
+                        readModifyWrite.addPoint(dim, num)
                 f.close()
 
-            plt.plot(read.x, read.y, linewidth = 1, 
-                    marker='o', markerfacecolor='blue', markersize=5, label="READ") 
-            plt.plot(update.x, update.y, linewidth = 1, 
-                    marker='o', markerfacecolor='blue', markersize=5, label="UPDATE")  
+            if(len(read.x)>0):
+                plt.plot(read.x, read.y, linewidth = 1, 
+                        marker='o', markerfacecolor='blue', markersize=5, label="READ") 
+            if(len(update.x)>0):
+                plt.plot(update.x, update.y, linewidth = 1, 
+                        marker='o', markerfacecolor='blue', markersize=5, label="UPDATE")  
+            
+            if(len(scan.x)>0):
+                plt.plot(scan.x, scan.y, linewidth = 1, 
+                    marker='o', markerfacecolor='blue', markersize=5, label="SCAN")  
+
+            if(len(readModifyWrite.x)>0):
+                plt.plot(readModifyWrite.x, readModifyWrite.y, linewidth = 1, 
+                    marker='o', markerfacecolor='blue', markersize=5, label="READ-MODIFY-WRITE")  
+
+
             # naming the x axis 
             plt.xlabel('Input dimension') 
             plt.xscale('log')
@@ -71,6 +92,9 @@ def generateAndShowThroughputLatencyPlots(input_dim, throughputs, clients, sourc
             # Generate plot Throughput / latency for every dim value
             read = Serie("read")
             update = Serie("update")
+            scan = Serie("scan")
+            readModifyWrite = Serie("Read-Modify-Write")
+
             for t in throughputs:
                 # new_path = os.path.join(cur_path, source ,"output-Run-dim" + str(dim) +".txt")
                 # new_path = os.path.relpath("\\output\\test-mongo-scenarioA\\outputRun-dim" + str(dim) +".txt", cur_path)
@@ -83,17 +107,35 @@ def generateAndShowThroughputLatencyPlots(input_dim, throughputs, clients, sourc
                         parts = line.split("[READ], AverageLatency(us), ")
                         num = float(parts[1])
                         read.addPoint(t, num)
-                    else:
-                        if "[UPDATE], AverageLatency(us)," in line:
-                            parts = line.split("[UPDATE], AverageLatency(us),")
-                            num = float(parts[1])
-                            update.addPoint(t, num)
+                    elif "[UPDATE], AverageLatency(us)," in line:
+                        parts = line.split("[UPDATE], AverageLatency(us),")
+                        num = float(parts[1])
+                        update.addPoint(t, num)
+                    elif "[SCAN], AverageLatency(us)," in line:
+                        parts = line.split("[SCAN], AverageLatency(us),")
+                        num = float(parts[1])
+                        scan.addPoint(t, num)
+                    elif "[READ-MODIFY-WRITE], AverageLatency(us)," in line:
+                        parts = line.split("[READ-MODIFY-WRITE], AverageLatency(us),")
+                        num = float(parts[1])
+                        readModifyWrite.addPoint(t, num)
                 f.close()
 
-            plt.plot(read.x, read.y, linewidth = 1, 
-                    marker='o', markerfacecolor='blue', markersize=5, label="READ") 
-            plt.plot(update.x, update.y, linewidth = 1, 
-                    marker='o', markerfacecolor='blue', markersize=5, label="UPDATE")  
+            if(len(read.x)>0):
+                plt.plot(read.x, read.y, linewidth = 1, 
+                        marker='o', markerfacecolor='blue', markersize=5, label="READ") 
+            if(len(update.x)>0):
+                plt.plot(update.x, update.y, linewidth = 1, 
+                        marker='o', markerfacecolor='blue', markersize=5, label="UPDATE")  
+            
+            if(len(scan.x)>0):
+                plt.plot(scan.x, scan.y, linewidth = 1, 
+                    marker='o', markerfacecolor='blue', markersize=5, label="SCAN")  
+
+            if(len(readModifyWrite.x)>0):
+                plt.plot(readModifyWrite.x, readModifyWrite.y, linewidth = 1, 
+                    marker='o', markerfacecolor='blue', markersize=5, label="READ-MODIFY-WRITE")  
+
             # naming the x axis 
             plt.xlabel('Throughput') 
             plt.xscale('log')
@@ -115,10 +157,14 @@ def generateAndShowInputLatencyPlotsComparison(input_dim, throughputs, clients, 
         for t in throughputs:
             reads=[]
             updates=[]
+            scans=[]
+            rmw=[]
             # Generate plot Input dim / latency for every throughput value
             for source in sources:
                 read = Serie(source)
                 update = Serie(source)
+                scan = Serie(source)
+                readModifyWrite = Serie(source)
                 cur_path = os.path.dirname(__file__)
                 cur_path = os.path.join(cur_path, source)
                 for dim in input_dim:
@@ -133,23 +179,44 @@ def generateAndShowInputLatencyPlotsComparison(input_dim, throughputs, clients, 
                             parts = line.split("[READ], AverageLatency(us), ")
                             num = float(parts[1])
                             read.addPoint(dim, num)
-                        else:
-                            if "[UPDATE], AverageLatency(us)," in line:
-                                parts = line.split("[UPDATE], AverageLatency(us),")
-                                num = float(parts[1])
-                                update.addPoint(dim, num)
+                        elif "[UPDATE], AverageLatency(us)," in line:
+                            parts = line.split("[UPDATE], AverageLatency(us),")
+                            num = float(parts[1])
+                            update.addPoint(dim, num)
+                        elif "[SCAN], AverageLatency(us)," in line:
+                            parts = line.split("[SCAN], AverageLatency(us),")
+                            num = float(parts[1])
+                            scan.addPoint(dim, num)
+                        elif "[READ-MODIFY-WRITE], AverageLatency(us)," in line:
+                            parts = line.split("[READ-MODIFY-WRITE], AverageLatency(us),")
+                            num = float(parts[1])
+                            readModifyWrite.addPoint(dim, num)
                     f.close()
                 reads.append(read)
                 updates.append(update)
+                scans.append(scan)
+                rmw.append(readModifyWrite)
 
             marker = itertools.cycle((',', '+', '.', 'o', '*', 's', 'x')) 
             for serie in reads:
-                plt.plot(serie.x, serie.y, linewidth = 1, 
-                        marker=next(marker), markerfacecolor='blue', markersize=5, label="READ - " + serie.name) 
+                if(len(serie.x)>0):
+                    plt.plot(serie.x, serie.y, linewidth = 1, 
+                            marker=next(marker), markerfacecolor='blue', markersize=5, label="READ - " + serie.name) 
             
             for serie in updates:
-                plt.plot(serie.x, serie.y, linewidth = 1, 
-                        marker=next(marker), markerfacecolor='blue', markersize=5, label="UPDATE - " + serie.name)  
+                if(len(serie.x)>0):
+                    plt.plot(serie.x, serie.y, linewidth = 1, 
+                            marker=next(marker), markerfacecolor='blue', markersize=5, label="UPDATE - " + serie.name)  
+                
+            for serie in scans:
+                if(len(serie.x)>0):
+                    plt.plot(serie.x, serie.y, linewidth = 1, 
+                            marker=next(marker), markerfacecolor='blue', markersize=5, label="SCAN - " + serie.name)  
+            for serie in rmw:
+                if(len(serie.x)>0):
+                    plt.plot(serie.x, serie.y, linewidth = 1, 
+                            marker=next(marker), markerfacecolor='blue', markersize=5, label="READ-MODIFY-WRITE - " + serie.name)  
+            
             # naming the x axis 
             plt.xlabel('Input dimension') 
             plt.xscale('log')
@@ -172,9 +239,13 @@ def generateAndShowThroughputLatencyPlotsComparison(input_dim, throughputs, clie
             # Generate plot Throughput / latency for every dim value
             reads=[]
             updates=[]
+            scans=[]
+            rmw=[]
             for source in sources:
                 read = Serie(source)
                 update = Serie(source)
+                scan = Serie(source)
+                readModifyWrite = Serie(source)
                 cur_path = os.path.dirname(__file__)
                 cur_path = os.path.join(cur_path, source)
                 for t in throughputs:
@@ -189,25 +260,46 @@ def generateAndShowThroughputLatencyPlotsComparison(input_dim, throughputs, clie
                             parts = line.split("[READ], AverageLatency(us), ")
                             num = float(parts[1])
                             read.addPoint(t,num)
-                        else:
-                            if "[UPDATE], AverageLatency(us)," in line:
-                                parts = line.split("[UPDATE], AverageLatency(us),")
-                                num = float(parts[1])
-                                update.addPoint(t,num)
+                        elif "[UPDATE], AverageLatency(us)," in line:
+                            parts = line.split("[UPDATE], AverageLatency(us),")
+                            num = float(parts[1])
+                            update.addPoint(t, num)
+                        elif "[SCAN], AverageLatency(us)," in line:
+                            parts = line.split("[SCAN], AverageLatency(us),")
+                            num = float(parts[1])
+                            scan.addPoint(t, num)
+                        elif "[READ-MODIFY-WRITE], AverageLatency(us)," in line:
+                            parts = line.split("[READ-MODIFY-WRITE], AverageLatency(us),")
+                            num = float(parts[1])
+                            readModifyWrite.addPoint(t, num)
                     f.close()
                 reads.append(read)
                 updates.append(update)
+                scans.append(scan)
+                rmw.append(readModifyWrite)
 
             marker = itertools.cycle((',', '+', '.', 'o', '*', 's', 'x')) 
 
             for serie in reads:
-                plt.plot(serie.x, serie.y, linewidth = 1, 
-                        marker=next(marker), markerfacecolor='blue', markersize=5, label="READ - " + serie.name) 
+                if(len(serie.x)>0):
+                    plt.plot(serie.x, serie.y, linewidth = 1, 
+                            marker=next(marker), markerfacecolor='blue', markersize=5, label="READ - " + serie.name) 
             
             for serie in updates:
-                plt.plot(serie.x, serie.y, linewidth = 1, 
-                        marker=next(marker), markerfacecolor='blue', markersize=5, label="UPDATE - " + serie.name)  
-                        
+                if(len(serie.x)>0):
+                    plt.plot(serie.x, serie.y, linewidth = 1, 
+                            marker=next(marker), markerfacecolor='blue', markersize=5, label="UPDATE - " + serie.name)  
+            
+            for serie in scans:
+                if(len(serie.x)>0):
+                    plt.plot(serie.x, serie.y, linewidth = 1, 
+                            marker=next(marker), markerfacecolor='blue', markersize=5, label="SCAN - " + serie.name)  
+
+            for serie in rmw:
+                if(len(serie.x)>0):
+                    plt.plot(serie.x, serie.y, linewidth = 1, 
+                            marker=next(marker), markerfacecolor='blue', markersize=5, label="READ-MODIFY-WRITE - " + serie.name)  
+                            
             # naming the x axis 
             plt.xlabel('Throughput') 
             plt.xscale('log')
