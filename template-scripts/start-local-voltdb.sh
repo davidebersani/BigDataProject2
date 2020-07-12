a04a5b87c8cb7b202ad0378cb0f74739266dfc81
Davide Bersani <davide.bersani96@gmail.com>
2020-07-12 16:36:56 +0200

    Added properties file for voltdb, modified script for init variables.

--- template-scripts/start-local-voltdb.sh
+++ template-scripts/start-local-voltdb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source =REPO_DIR=/scripts/common.sh
 docker pull voltdb/voltdb-community:latest
 if [ $# -eq 0 ] 
 then
@@ -20,3 +21,6 @@ else
         docker run -d -P -e HOST_COUNT=$1 -e HOSTS=$host_ele --name=node$c --network=voltLocalCluster voltdb/voltdb-community:latest >> id_voltdb.txt
     done
 fi
+
+port=$(docker port node1 21212 | cut -d':' -f2)
+sed "s@=VOLTDB_MAPPED_PORT=@$port@g" $SCRIPTS/voltdb-prop-template > $SCRIPTS/voltdb-prop
