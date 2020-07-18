78959c3e28938d093c6aea5b89bc353a7699c0a2
andrea19081996 <33671536+andrea19081996@users.noreply.github.com>
2020-07-18 21:50:02 +0200

    file to move for jdk11

---
+++ cloud/voltdb/javajdk.sh
@@ -0,0 +1,5 @@
+export J2SDKDIR=/usr/lib/jdk/jdk-11.0.8
+export J2REDIR=/usr/lib/jdk/jdk-11.0.8/jre
+export PATH=$PATH:/usr/lib/jdk/jdk-11.0.8/bin:/usr/lib/jdk/jdk-11.0.8/db/bin:/usr/lib/jdk/jdk-11.0.8/jre/bin
+export JAVA_HOME=/usr/lib/jdk/jdk-11.0.8
+export DERBY_HOME=/usr/lib/jdk/jdk-11.0.8/db
