467188ec5760c2f2c392792d637aa5de52ec34e5
andrea19081996 <33671536+andrea19081996@users.noreply.github.com>
2020-07-19 15:51:36 +0200

    Add files via upload

--- cloud/voltdb/voltdb-cloud.sh
+++ cloud/voltdb/voltdb-cloud.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 sudo pip install pathlib
-sudo tar -zxvf voltdb-developer-9.3.1.tar.gz -C /opt
-cd /opt
-sudo mv voltdb-developer-9.3.1 voltdb
-cd /home/ec2-user
-sudo mkdir /usr/lib/jdk
-sudo cp jdk-11.0.8_linux-x64_bin.tar.gz /usr/lib/jdk/jdk-11.0.8_linux-x64_bin.tar.gz
-cd /usr/lib/jdk
-sudo tar -zxvf jdk-11.0.8_linux-x64_bin.tar.gz
-sudo rm jdk-11.0.8_linux-x64_bin.tar.gz
-sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jdk/jdk-11.0.8/bin/java" 1010
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jdk/jdk-11.0.8/bin/javac" 1010
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jdk/jdk-11.0.8/bin/javaws" 1010
-cd /home/ec2-user
-sudo mv javajdk.sh /etc/profile.d
-unzip BigDataProject2-master.zip
+sudo tar -zxvf /home/ec2-user/voltdb-developer-9.3.1.tar.gz
+sudo rm -r /home/ec2-user/voltdb-developer-9.3.1.tar.gz
+sudo mv /home/ec2-user/voltdb-developer-9.3.1 /home/ec2-user/voltdb
+sudo yum install java-1.8.0
+sudo yum remove java-1.7.0-openjdk
